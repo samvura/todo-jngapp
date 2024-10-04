5769cbaae9aba52a7729120b61608166577fe9b4
samvura <samvura.mpofu@gmail.com>
2024-10-04 13:10:34 +0100

    Todo App Final

--- todo/urls.py
+++ todo/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('mark_incomplete/<int:pk>/', views.mark_incomplete, name='mark_incomplete'),
     # Edit Feature
     path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
+    # Delete task
+    path('delete_task/<int:pk>/', views.delete_task, name='delete_task'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -35,3 +35,8 @@ def edit_task(request, pk):
             'get_task': get_task,
         }
     return render(request, 'edit_task.html', context)
+
+def delete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
+    return redirect('home')
